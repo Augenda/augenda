c6ad49382cf3fc35ec3bd9e7eb4561e2db77270a
Stefany Suzany Gomes da Silva <107081295+StefanySuzany@users.noreply.github.com>
2024-10-23 22:09:55 -0300

    Limpeza do template
    Removendo os detalhes do template do React

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-
-        TESTE
-        </p>
-        <a>Teste Eduardo oioioioioioi</a>
-         TESTE TESTE TESTE
-
-         TESTE 2 SEM SUBSTITUIR
-        <a>
-          Não substuir
-        </a>
       </header>
     </div>
   );
