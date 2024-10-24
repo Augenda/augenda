f2dd1c96c074d66ae99d55ac88c458a4fd54851a
Eduardo Nunes Paiva <eduardonp03@gmail.com>
2024-10-23 21:43:12 -0300

    Update App.js
    teste feature-eduardo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         TESTE BRANCH STEFANY
+         TESTE TESTE TESTE
         </p>
       </header>
     </div>
