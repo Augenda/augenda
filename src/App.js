1d551bb863bc9b0608a5ebe2f80fc16cd573c637
Eduardo Nunes Paiva <eduardonp03@gmail.com>
2024-10-23 21:56:15 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         TESTE TESTE TESTE
+        TESTE
         </p>
+        <a>Teste Eduardo oioioioioioi</a>
       </header>
     </div>
   );
